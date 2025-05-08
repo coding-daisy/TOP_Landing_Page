c9b7a93d2e9f7614b702e0ae84add93d02ac09c7
coding-daisy <simplydaisyy.101@gmail.com>
2025-05-08 12:32:05 +0200

    Implement buttons for buying and signing up

---
+++ script.java
@@ -0,0 +1,11 @@
+
+function signUp() {
+    let signUpButtonOne = document.getElementById('sign-up-button-one');
+    let signUpButtonTwo = document.getElementById('sign-up-button-two');
+    
+    signUpButtonOne.innerHTML = "Signed up!";
+    signUpButtonTwo.innerHTML = "Signed up!";
+    signUpButtonOne.style.color = "#CCCCCC";
+    signUpButtonTwo.style.color = "#CCCCCC";
+    signUpButtonTwo.style.borderColor = "#CCCCCC";
+}
